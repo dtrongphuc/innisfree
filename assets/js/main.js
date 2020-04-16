0a91682567aa498e53644efb431fc1402219b325
dtrongphuc <dtrongphucdv@gmail.com>
2020-04-16 19:55:15 +0700

    finnished innistar

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,15 +99,34 @@ document.querySelectorAll('.slide-icon').forEach(icon => {
 
 // innistar
 
-var wideWidth = document.querySelector('.grid.wide').offsetWidth;
+// var wideWidth = document.querySelector('.grid.wide').offsetWidth;
 
-var getWidth = wideWidth/5;
-var listBox = document.querySelectorAll('.star-box');
-function setWidth() {
-    for(let i = 0 ; i < listBox.length ; ++i) {
-        listBox[i].style.width = `${getWidth}px`;
-        listBox[i].style.height = `${getWidth}px`;
-    }
+// var getWidth = wideWidth/5;
+// var listBox = document.querySelectorAll('.star-box');
+// function setWidth() {
+//     for(let i = 0 ; i < listBox.length ; ++i) {
+//         listBox[i].style.width = `${getWidth}px`;
+//         listBox[i].style.height = `${getWidth}px`;
+//     }
+// }
+
+// setWidth();
+
+var innistarListBg = [
+    'https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/93244090_226708491914223_436196715629151101_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=5nEAy9yjHI4AX-C6us9&oh=da9071917da47ade91fd1047b01fe4eb&oe=5EC11582',
+    'https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/93521084_2279788825657853_1669851788923943449_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=4zhEuqq2b18AX_RjJI0&oh=201d4757e411431fffc544a77e728dbe&oe=5EC22207',
+    'https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/92885897_598523020739272_8816635138965694160_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=HiSpmMh__qwAX_O_j86&oh=688d8ded822873dbdd758ce8d56447e1&oe=5EC263F5',
+    'https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/93803540_222029775530313_3380555312185870083_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=109&_nc_ohc=fe-SpkyWmbEAX_5WvMP&oh=8ccd719c3c188764a000eb4c2047bb1a&oe=5EBEC18A',
+    'https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/92562905_713045709433150_2612755459678033801_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=lnwp_pmmVgsAX-i9_-E&oh=d79a3ba92b2534741332d49cbbd639bd&oe=5EC159B3',
+    'https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/92566032_223971068822497_5702065511642632695_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=4oruk-rPcHYAX__osxH&oh=0bc67d2c2aecf67c6e7afe530b8a95ac&oe=5EC1B26C',
+    'https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/93565948_2630028240576547_7867685382297311923_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=107&_nc_ohc=_qTpkxUPmt4AX_ukX4E&oh=39fbaed4f9bf2e24a62aef1df7cf9aaf&oe=5EBEA22D'
+]
+
+function renderBg() {
+    let starBgList = document.querySelectorAll('.innistar-bg');
+    starBgList.forEach((item, index) => {
+        item.style.backgroundImage = `url(${innistarListBg[index]})`;
+    });
 }
 
-setWidth();
+renderBg();
